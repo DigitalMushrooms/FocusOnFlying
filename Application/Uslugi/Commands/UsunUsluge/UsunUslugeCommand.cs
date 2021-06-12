8cf5ec841696abe42089c5264439f9cbd4c96929
Łukasz Grzybowski <lukasz.grzybowski.pl@gmail.com>
2021-06-12 15:51:41 +0200

    Dodanie powiadomienia klienta mailowo o anulowaniu usługi.

--- Application/Uslugi/Commands/UsunUsluge/UsunUslugeCommand.cs
+++ Application/Uslugi/Commands/UsunUsluge/UsunUslugeCommand.cs
@@ -3,6 +3,7 @@ using FocusOnFlying.Domain.Entities.FocusOnFlyingDb;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,20 +17,34 @@ namespace FocusOnFlying.Application.Uslugi.Commands.UsunUsluge
     public class UsunUslugeCommandHandler : IRequestHandler<UsunUslugeCommand>
     {
         private readonly IFocusOnFlyingContext _focusOnFlyingContext;
+        private readonly IMailService _mailService;
 
-        public UsunUslugeCommandHandler(IFocusOnFlyingContext focusOnFlyingContext)
+        public UsunUslugeCommandHandler(IFocusOnFlyingContext focusOnFlyingContext, IMailService mailService)
         {
             _focusOnFlyingContext = focusOnFlyingContext;
+            _mailService = mailService;
         }
 
         public async Task<Unit> Handle(UsunUslugeCommand request, CancellationToken cancellationToken)
         {
             Usluga usluga = await _focusOnFlyingContext.Uslugi.SingleAsync(x => x.Id == request.Id);
+            Klient klient = await _focusOnFlyingContext.Klienci.SingleAsync(x => x.Id == usluga.IdKlienta);
+
             _focusOnFlyingContext.Uslugi.Remove(usluga);
 
             await _focusOnFlyingContext.SaveChangesAsync();
 
+            await _mailService.WyslijWadomoscEmail(klient.Email, "Usunięto usługę", WygenerujOpisWiadomosci());
+
             return Unit.Value;
+        }
+
+        private string WygenerujOpisWiadomosci()
+        {
+            var trescWiadomosci = $@"Dzień dobry,<br/>
+                    Pragniemy poinformować, że zlecona przez Ciebie usługa została anulowana w naszym systemie.<br/>";
+
+            return trescWiadomosci;
         }
     }
 }
